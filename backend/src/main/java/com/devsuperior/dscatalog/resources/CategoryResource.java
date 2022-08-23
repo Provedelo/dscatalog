601644b79ada66e31476924b02820872af880f4d
Angelo Provedelo <angelo_provedelo@hotmail.com>
2022-08-23 15:10:04 -0300

    DTO implementado

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryServices;
 
 @RestController //Usa recurso do controlador Rest ja implementada, pré-procesamento
@@ -20,8 +20,8 @@ public class CategoryResource { //implementa o recurso do rest, a API controlado
 	private CategoryServices service;
 	
 	@GetMapping										  //define o webservice, um endpoint do recurso
-	public ResponseEntity<List <Category>> findAll(){ //ResponseEntity é um obj que encapsula uma resposta http
-		List<Category> list = service.findall();
+	public ResponseEntity<List <CategoryDTO>> findAll(){ //ResponseEntity é um obj que encapsula uma resposta http
+		List<CategoryDTO> list = service.findall();
 		return ResponseEntity.ok().body(list);		   //retorna requisiçao e corpo(e sujeito)
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryServices.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryServices.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +19,14 @@ public class CategoryServices {
 	private CategoryRepository repository; //inj de depencia do CategoryRepository
 	
 	@Transactional(readOnly = true) //readyonly para somente leitura e ser mais rapido
-	public List<Category> findall(){
-		return repository.findAll();
+	public List<CategoryDTO> findall(){
+		List<Category> list = repository.findAll();
+		
+		//resumo em expressao lamba, menos verboso
+		return list.stream().map(x -> new CategoryDTO(x))
+		.collect(Collectors.toList());
+		
+
 	}
 	
 }
@@ -26,3 +35,12 @@ public class CategoryServices {
 //@Component para genericos sem significado direto, para injeção de dependencia
 //@Autowired, faz injecao de dependencia automatica, instancia controlada pelo spring
 //@Transactional, garante a transacao no banco de dados,  orquestrado pela spring, faz tudo ou nada na propriedade ACID
+
+/*List<CategoryDTO> listDTO = new ArrayList<>();
+for(Category obj : list) {
+	listDTO.add(new CategoryDTO(obj));
+}
+
+return listDTO;
+//metodo verboso para implementação de varredura de obj de classe para classe dto
+*/
